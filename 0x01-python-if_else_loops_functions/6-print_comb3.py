1dad5c1077f9c630f7eab1d0339c38869b8d986a
MasunguloSilubane <masungulogodi3@gmail.com>
2022-06-30 06:48:59 +0000

    Task 6

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,14 @@
 #!/usr/bin/python3
-for i in range(10):
-    for j in range(i + 1, 10):
-        print(
-            "{}{}".format(i, j),
-            end=", " if int(str(i) + str(j)) < 89 else "\n"
-            )
+digit1 = 0
+digit2 = 1
+while digit1 <= 8:
+    while digit2 <= 9:
+        if digit1 != digit2:
+            print("{:d}".format(digit1), end='')
+            if digit1 != 8:
+                print("{:d}, ".format(digit2), end='')
+            else:
+                print("{:d}".format(digit2))
+        digit2 += 1
+    digit2 = digit1 + 1
+    digit1 = digit1 + 1
