c3f4d831860cc365970a3f80c9715a7c50315201
MasunguloSilubane <masungulogodi3@gmail.com>
2022-07-28 08:28:22 +0000

    Task 0

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,50 +1,12 @@
 #!/usr/bin/python3
-"""A module to add two numbers
-
-This module performs the addition operation between two numbers,
-these numbers can be integers or floats.
-
-"""
-
-
 def add_integer(a, b=98):
-    """Adds two numbers
-
-    Performs the addition between two numbers.
-
-    Args:
-        a (:obj:`int, float`): The first number.
-        b (:obj:`int, float`, optional): The second number.
-
-    Returns:
-        int: The result of the addition.
-
-    """
-    if type(a) not in (int, float):
+    """ Function that adds 2 integers. """
+    if type(a) not in [int, float]:
         raise TypeError('a must be an integer')
-
-    if type(b) not in (int, float):
+    elif type(b) not in [int, float]:
         raise TypeError('b must be an integer')
-
-    a = convert_to_int(a)
-    b = convert_to_int(b)
-    return a + b
-
-
-def convert_to_int(num):
-    """Cast the data type of num parameter
-
-    Convert a float number to a integer number
-
-    Args:
-        num (:obj:`int, float`): The number to cast.
-
-    Returns:
-        int: The number casted to integer.
-
-    """
-    if type(num) is float:
-        num = int(num)
-        return num
-
-    return num
+    else:
+        if abs(a + b) == float('inf'):
+            raise OverflowError('overflow error')
+        else:
+            return int(a) + int(b)
