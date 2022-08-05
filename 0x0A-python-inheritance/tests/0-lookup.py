7306b728c74a04ceb1c123b938ddf6fd2dc5246f
MasunguloSilubane <masungulogodi3@gmail.com>
2022-08-05 03:58:50 -0700

    Task 1

--- 0x0A-python-inheritance/tests/0-lookup.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-
-'''Task 00 - 0. Lookup'''
-
-
-def lookup(obj):
-    '''Function that returns the list of available attributes
-    and methods of an object'''
-    return dir(obj)
