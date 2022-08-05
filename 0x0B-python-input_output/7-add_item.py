24dbdea1a44a29229e90f292625cb221c7f93d46
MasunguloSilubane <masungulogodi3@gmail.com>
2022-08-05 05:22:36 -0700

    Task 7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
-from json import dumps
-
-
 def save_to_json_file(my_obj, filename):
-        with open(filename, mode='w', encoding='utf-8') as f:
-                    f.write(dumps(my_obj))
+        """
+                function that writes an Object to a text file,
+                        using a JSON representation
+                            """
+                                import json
+                                    with open(filename, 'w') as f:
+                                                json.dump(my_obj, f)
