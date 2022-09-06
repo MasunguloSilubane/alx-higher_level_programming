02c9585a4520f387f6911d1a503a87472414ee3b
MasunguloSilubane <masungulogodi3@gmail.com>
2022-09-06 08:18:02 -0700

    Task19

--- 0x0D-SQL_introduction/102-top_city.sql
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -1,5 +1,5 @@
--- Displays the max temperature of each state, ordered by state name.
-SELECT `state`, MAX(`value`) AS `max_temp`
-FROM `temperatures`
-GROUP BY `state`
-ORDER BY `state`;
+-- Displays the 3 cities with the highest average
+-- temperatures between July and August.
+SELECT `city`, AVG(`value`) AS `avg_temp` FROM `temperatures`
+WHERE `month` = 7 OR `month` = 8 GROUP BY `city`
+ORDER BY `avg_temp` DESC LIMIT 3;
