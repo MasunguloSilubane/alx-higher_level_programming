53e551809e2c25663469c1f26d040b3debeceb62
MasunguloSilubane <masungulogodi3@gmail.com>
2022-10-08 05:44:32 -0700

    1-filter

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,27 +1,31 @@
 #!/usr/bin/python3
 
-import MySQLdb
+
 from sys import argv
+import MySQLdb
 
 
-def mysqlconnnect():
-        """
-            Connecting and quering data base
-                """
-                    try:
-                                db_connection = MySQLdb.connect(host="localhost", port=3306,
-                                                                                user=argv[1], passwd=argv[2],
-                                                                                                                        db=argv[3], charset="utf8")
-                                                                                                                            except Exception:
-                                                                                                                                        print("can't connect to database")
-                                                                                                                                                return 0
-                                                                                                                                                cur = db_connection.cursor()
-                                                                                                                                                    cur.execute("SELECT * FROM states ORDER BY id ASC")
-                                                                                                                                                        query_rows = cur.fetchall()
-                                                                                                                                                            for row in query_rows:
-                                                                                                                                                                    print(row)
-                                                                                                                                                                        cur.close()
-                                                                                                                                                                            db_connection.close()
+def connectDB():
+    """
+    Connect to database and quering
+    """
+    try:
+        db_connection = MySQLdb.connect(host="localhost", port=3306,
+                                            user=argv[1], password=argv[2],
+                                                                        db=argv[3], charset="utf8")
+    except Exception:
+        print("Can't connect to database")
+        return 0
+    cur = db_connection.cursor()
+    sql = "SELECT id, name FROM states ORDER BY id ASC;"
+    cur.execute(sql)
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        if 'N' == row[1][0]:
+            print(row)
+            cur.close()
+            db_connection.close()
 
 
-                                                                                                                                                                            mysqlconnnect()
+            connectDB()
+
