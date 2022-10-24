fe89a1e61faecefa5203b825938a9c35882f7f4b
MasunguloSilubane <masungulogodi3@gmail.com>
2022-10-24 04:46:30 -0700

    Task 0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,19 +1,11 @@
 #!/usr/bin/python3
-"""Fetches the URL: https://intranet.hbtn.io/status
-"""
-
-from urllib.request import Request, urlopen
-
+"""fetches https://intranet.hbtn.io/status"""
+from urllib import request
 
 if __name__ == "__main__":
-        req = Request('https://intranet.hbtn.io/status')
-
-with urlopen(req) as res:
+with request.urlopen('https://intranet.hbtn.io/status') as res:
 content = res.read()
-utf8_content = content.decode('utf-8')
-
-print('Body response:')
-print('\t- type: {_type}'.format(_type=type(content)))
-print('\t- content: {_content}'.format(_content=content))
-print('\t- utf8 content: {_utf8_c}'.format(_utf8_c=utf8_content))
-
+print("Body response:")
+print("\t- type: {}".format(type(content)))
+print("\t- content: {}".format(content))
+print("\t- utf8 content: {}".format(content.decode('utf-8')))
